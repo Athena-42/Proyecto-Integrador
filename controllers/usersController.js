b81f21d08965c3d183bd7a9986acc7883ddce765
Sophie <sandovalsofi99@gmail.com>
2022-11-18 14:57:56 -0300

    creando infraestructura de registro

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,14 +1,28 @@
 let db = require("../database/models")
 const moviesController = {
-    registerForm: function(req, res, next){
+    registerForm: function(req, res){
         res.render('register');
       },
-    loginForm: function(req, res, next) {
+    loginForm: function(req, res) {
  
         res.render('login');
           
+      },
+      register: function(req, res, next){
+        let errors = validationResult(req);
+        if (errors.isEmpty()) {
+          db.Usuarios.create({
+            name: req.body.name,
+            email: req.body.email,
+          })}else{res.render('', {errors: errors.mapped(), old: req.body})}  
+          },
+        
+         
+      login: function(req, res){
+        res.send('Logeado')
       }
-    }
+      }
+    
       
 
 module.exports = moviesController
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
 let usersController = require('../controllers/usersController')
+const { check } = require('express-validator');
+let validateMovie = [
+    check('title')
+    .notEmpty().withMessage('Debes completar el título')
+    .isLength({min: 2}).withMessage('El nombre debe contener al menos 2 caracteres'),
+    check('rating')
+    .notEmpty().withMessage('Debes completar el rating')
+    .isFloat({min: 0.0,max:10.0}).withMessage('El rating debe estar entre 0 y 10'),
+    check('awards')
+    .notEmpty().withMessage('Debes completar la cantidad de premios')
+    .isInt({min:0}).withMessage('La cantidad de premios no puede ser negativa'),
+    check('length')
+    .notEmpty().withMessage('Debes completar la duración de la película cantidad de en minutos')
+    .isFloat({min:0}).withMessage('Debes completar la duración de la película en cantidad de minutos'),
+    check('date')
+    .notEmpty().withMessage('Debes ingresar la fecha de estreno')
+];
 
 router.get('/register', usersController.registerForm);
 router.get('/login', usersController.loginForm);
+router.post('/register', usersController.register);
+router.post('/login', usersController.login);
 
 module.exports = router;
